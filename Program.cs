92a9cf5638be115ac59059dddfe9a7af6e6f33ae
Vinicius Jose <viniciuswk@gmail.com>
2022-12-26 19:55:40 -0300

    Página Deletar

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using ProjetoMVC.Context;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +9,7 @@ builder.Services.AddDbContext<AgendaContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
 
 builder.Services.AddControllersWithViews();
+
 
 var app = builder.Build();
 
